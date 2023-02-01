3ca09a6774e5ba1f22c83103504853b2046d5fa3
Jordan <Jordanholm12@gmail.com>
2023-01-31 21:57:02 -0700

    theme changer added, need to add button

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+/** @jsxImportSource theme-ui */
+import React, { useState, useEffect } from "react";
+import { useColorMode } from "theme-ui";
 import { Routes, Route } from 'react-router-dom';
 import MainNavbar from './components/shared/MainNavbar';
 import Nomatch from './components/shared/Nomatch';
@@ -6,6 +9,22 @@ import About from './components/about/About';
 import Contact from './components/contact/Contact';
 
 const App = () => {
+
+  const [ colorMode, setColorMode ] = useColorMode();
+  const [ url, setUrl ] = useState("None")
+  
+  useEffect( () => {
+        setUrl( window.location.href )
+        const checkForColorMode = () => {
+            if ( url === "http://localhost:3000/about" || url === "https://jordanholm.netlify.app/about") {
+                setColorMode(colorMode === 'light')
+                console.log(url)
+            } else {
+                setColorMode(colorMode === 'dark')
+            }
+        }
+        checkForColorMode()
+    }, [])
   
   return (
     <>
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'theme-ui';
+import theme from "./components/shared/Theme";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
